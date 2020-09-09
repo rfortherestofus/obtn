143f88c2455627eafb5d08719d309710b67a8117
David Keyes <dgkeyes@gmail.com>
2020-09-09 14:26:43 -0700

    Updates

--- R/obtn_plot_race_ethnicity_bar_chart.R
+++ R/obtn_plot_race_ethnicity_bar_chart.R
@@ -43,7 +43,7 @@ obtn_plot_race_ethnicity_bar_chart <- function(obtn_year,
       TRUE ~ tfff_dark_gray
     ))
 
-  if (geography_to_plot %in% obtn_oregon_counties) {
+  # if (geography_to_plot %in% obtn_oregon_counties) {
     ggplot2::ggplot(obtn_race_ethnicity_filtered,
                     ggplot2::aes(x = population, y = value)) +
       ggplot2::geom_bar(stat = "identity", fill = tfff_dark_green) +
@@ -59,39 +59,42 @@ obtn_plot_race_ethnicity_bar_chart <- function(obtn_year,
                                       label = stringr::str_glue("{population}: {pct_formatted}")),
                          hjust = 1,
                          size = 2.75,
-                         color = "white",
-                         family = "Calibri") +
-      ggplot2::scale_x_discrete(expand = c(0, 0)) +
-      ggplot2::scale_y_continuous(expand = c(0, 0)) +
-      ggplot2::theme_void() +
-      ggplot2::coord_flip()
-
-  } else {
-    ggplot2::ggplot(obtn_race_ethnicity_filtered,
-                    ggplot2::aes(x = population, y = value)) +
-      ggplot2::geom_bar(stat = "identity", fill = tfff_dark_green) +
-      ggplot2::geom_text(data = dplyr::filter(obtn_race_ethnicity_filtered, population != "White"),
-                         ggplot2::aes(population, value + .025,
-                                      label = stringr::str_glue("{population}: {pct_formatted}")),
-                         hjust = 0,
-                         size = 2.75 * 1.5,
-                         color = tfff_dark_gray,
-                         family = "Calibri") +
-      ggplot2::geom_text(data = dplyr::filter(obtn_race_ethnicity_filtered, population == "White"),
-                         ggplot2::aes(population, value - .025,
-                                      label = stringr::str_glue("{population}: {pct_formatted}")),
-                         hjust = 1,
-                         size = 2.75 * 1.5,
                          color = "white",
                          family = "Calibri") +
       ggplot2::scale_x_discrete(expand = c(0, 0)) +
       ggplot2::scale_y_continuous(expand = c(0, 0),
-                                  limits = c(0, 0.85)) +
+                                  limits = c(0, 1)) +
       ggplot2::theme_void() +
       ggplot2::coord_flip()
-  }
 
-obtn_save_plot(obtn_year, "Race Ethnicity Bar Chart", geography_to_plot, plot_width, plot_height)
+  # } else {
+  #   ggplot2::ggplot(obtn_race_ethnicity_filtered,
+  #                   ggplot2::aes(x = population, y = value)) +
+  #     ggplot2::geom_bar(stat = "identity", fill = tfff_dark_green) +
+  #     ggplot2::geom_text(data = dplyr::filter(obtn_race_ethnicity_filtered, population != "White"),
+  #                        ggplot2::aes(population, value + .025,
+  #                                     label = stringr::str_glue("{population}: {pct_formatted}")),
+  #                        hjust = 0,
+  #                        size = 2.75 * 1.5,
+  #                        color = tfff_dark_gray,
+  #                        family = "Calibri") +
+  #     ggplot2::geom_text(data = dplyr::filter(obtn_race_ethnicity_filtered, population == "White"),
+  #                        ggplot2::aes(population, value - .025,
+  #                                     label = stringr::str_glue("{population}: {pct_formatted}")),
+  #                        hjust = 1,
+  #                        size = 2.75 * 1.5,
+  #                        color = "white",
+  #                        family = "Calibri") +
+  #     ggplot2::scale_x_discrete(expand = c(0, 0)) +
+  #     ggplot2::scale_y_continuous(expand = c(0, 0),
+  #                                 limits = c(0, 0.85)) +
+  #     ggplot2::theme_void() +
+  #     ggplot2::coord_flip()
+  # }
+
+# obtn_save_plot(obtn_year, "Race Ethnicity Bar Chart", geography_to_plot, plot_width, plot_height)
 
 }
 
+
+obtn_plot_race_ethnicity_bar_chart(2020, "Multnomah")
